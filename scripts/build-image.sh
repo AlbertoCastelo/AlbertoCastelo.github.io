1b274342f9d113891578afd1bb7469d19eba7ed8
Alberto Castelo <alberto.castelo.becerra@gmail.com>
2020-02-02 17:38:04 +0100

    some changes

---
+++ scripts/build-image.sh
@@ -0,0 +1,4 @@
+docker build -t jekyll-blog .
+
+# docker run -d -v /webroot:/var/www/html -p 4000:4000 --name blog jekyll-blog 
+docker run -it -p 4000:4000 --name blog -v "$PWD":/srv/jekyll jekyll-blog 
